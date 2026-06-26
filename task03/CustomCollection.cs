0f47b186e56dcf731783ac79abdee25dc3980b32
Irina Gorchakova <g.oleg68550207@gmail.com>
2026-06-26 22:46:31 +0600

    test(task03): добавлены тесты IteratorTests ( реализация не готова)

---
+++ task03/CustomCollection.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CustomCollection<T> : IEnumerable<T>
+{
+    private readonly List<T> _items = new();
+
+    public void Add(T item) => _items.Add(item);
+    public void Remove(T item) => _items.Remove(item);
+
+    public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    
+    public IEnumerable<T> GetReverseEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public static IEnumerable<int> GenerateSequence(int start, int count)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public IEnumerable<T> FilterAndSort(Func<T, bool> predicate, Func<T, IComparable> keySelector)
+    {
+        throw new NotImplementedException();
+    }
+}
