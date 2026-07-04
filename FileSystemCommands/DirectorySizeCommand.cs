38c97da91f01dcf3f9549a53ae2d7936b8e94ea7
Irina Gorchakova <g.oleg68550207@gmail.com>
2026-07-05 01:02:17 +0600

    feat(task08): добавлены команды DirectorySizeCommand и FindFilesCommand

---
+++ FileSystemCommands/DirectorySizeCommand.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+using System.Linq; 
+
+public class DirectorySizeCommand: ICommand
+{
+    private readonly string _path;
+    public DirectorySizeCommand(string path)
+    {
+        _path=path;
+    }
+
+    public void Execute()
+    {
+        if(!Directory.Exists(_path))
+        {
+            Console.WriteLine($"Каталог {_path} не найден");
+            return;
+        }
+        var files = Directory.GetFiles(_path,"*",SearchOption.AllDirectories);
+        long filesSize= 0;
+        foreach (var file in files)
+        {
+            filesSize+= new FileInfo(file).Length;
+        }
+        Console.WriteLine($"Размер каталога: {filesSize}");
+    }
+}
